19f7c72f9beb6f6a27fddf2aad836c1bccc51856
fuxiaokun <fxk49@126.com>
2017-06-25 11:04:46 +0800

    remove xunit，and then use Visual Studio UnitTest

---
+++ cake.strongnametool.test/StrongNameResolverTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Cake.StrongNameTool.Test.Fixture;
+using Cake.Core;
+using Cake.Core.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cake.strongnametool.test
+{
+    [TestClass]
+    public class StrongNameResolverTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Should_Throw_If_File_System_Is_Null()
+        {
+            // Given
+            var fixture = new StrongNameResolverFixture();
+            fixture.FileSystem = null;
+
+            fixture.Resolve();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Should_Throw_If_Environment_Is_Null()
+        {
+            // Given
+            var fixture = new StrongNameResolverFixture();
+            fixture.Environment = null;
+
+            fixture.Resolve();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Should_Throw_If_Registry_Is_Null()
+        {
+            // Given
+            var fixture = new StrongNameResolverFixture();
+            fixture.Registry = null;
+            fixture.Resolve();
+        }
+    }
+
+    [TestClass]
+    public sealed class TheResolveMethod
+    {
+        [TestMethod]
+        public void Should_Return_From_Disc_If_Found(bool is64Bit)
+        {
+            // Given
+            var fixture = new StrongNameResolverFixture(is64Bit);
+            fixture.GivenThatToolExistInKnownPath();
+
+            // When
+            var result = fixture.Resolve();
+
+            // Then
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Should_Return_From_Registry_If_Found()
+        {
+            // Given
+            var fixture = new StrongNameResolverFixture();
+            fixture.GivenThatToolHasRegistryKey();
+
+            // When
+            var result = fixture.Resolve();
+
+            // Then
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CakeException))]
+        public void Should_Throw_If_Not_Found_On_Disc_And_SDK_Registry_Path_Cannot_Be_Resolved()
+        {
+            // Given
+            var fixture = new StrongNameResolverFixture();
+            fixture.GivenThatNoSdkRegistryKeyExist();
+
+            fixture.Resolve();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CakeException))]
+        public void Should_Throw_If_SignTool_Cannot_Be_Resolved()
+        {
+            // Given
+            var fixture = new StrongNameResolverFixture();
+            fixture.Resolve();
+        }
+    }
+}
